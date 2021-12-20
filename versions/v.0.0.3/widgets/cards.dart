5d4a5409c0d62632532ff6ea0a032dc0143e56d9
Kerem Alan <kerem.alan@ogr.sakarya.edu.tr>
2021-12-20 23:47:57 +0300

    Add widgets folder, card widgets.
    Add widgets folder.
    Add texts.dart contains text wigdets.
    Add buttons.dart contains button widgets.
    Add cards.dart contains card widgets.
    Add FilledButton widget in buttons.dart.
    Add CategoryCard widget in cards.dart.
    Add CategoryTitle widget in cards.dart.
    Add CategoryTermCard in cards.dart.
    Change Padding > SingleChildScrollView in main.dart.
    Add actions in LugatAppBar in main.dart.

---
+++ versions/v.0.0.3/widgets/cards.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/cupertino.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:flutter/material.dart';
+import 'texts.dart';
+import 'buttons.dart';
+
+Widget PopularCategoryCard(categoryImgUrl, categoryName) {
+  return Padding(
+    padding: const EdgeInsets.only(right: 10),
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: HexColor("#BEBEBE"),
+          width: 0.5,
+        ),
+        borderRadius: BorderRadius.all(
+          Radius.circular(6),
+        ),
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Image.network(
+                "${categoryImgUrl}",
+                height: 50,
+                width: 50,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 4, left: 4, bottom: 7),
+            child: Caption2Text("${categoryName}", "#BEBEBE"),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget CategoryCard(categoryName, categoryTermValue, categoryImageUrl) {
+  return Padding(
+    padding: const EdgeInsets.only(top: 22.0),
+    child: Container(
+      height: 330,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(6),
+        image: DecorationImage(
+          image: NetworkImage("${categoryImageUrl}"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16, top: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Caption2Text("Günün kelimesi", "#FFFFFF"),
+                    Caption2Text("Tipografi", "#FFFFFF"),
+                  ],
+                ),
+                FilledButton("#FFFFFF", "Kelime ekle"),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16, bottom: 16, left: 16),
+            child: Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    HeadlineText("${categoryName}", "#FFFFFF"),
+                    Caption1Text(
+                        "${categoryTermValue} terim içeriyor", "#FFFFFF"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget CategoryTitle(categoryName) {
+  return Container(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        HeadlineText("${categoryName}", "#000000"),
+        TextButton(
+            onPressed: () {}, child: Caption1Text("Tümünü gör", "#001FC6")),
+      ],
+    ),
+  );
+}
+
+Widget CategoryTermCard(termImageUrl, termName, termAuthorName) {
+  return Container(
+    child: Padding(
+      padding: const EdgeInsets.only(right: 10),
+      child: Column(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(9),
+                child: Image.network(
+                  "${termImageUrl}",
+                  height: 124,
+                  width: 124,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Caption2Text("${termName}", "#000000"),
+                    Caption1Text("${termAuthorName}", "#BEBEBE"),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
+}
